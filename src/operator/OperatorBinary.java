33c43b4dc62b117caee2067a78db5cbf13b9ff14
dyahdwihandayani <62831904+dyahdwihandayani@users.noreply.github.com>
2020-04-28 11:44:48 +0700

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+//menempatkan nilai kedalam variable
+//baris 5-7 adalah comment/komentar yang tidak di jalankan pleh program
+
+    String nama = "Dyah Dwi Handayani";
+    int jumlahSks =24;
+    double indeksPrestasiKumulatif = 3.48;
+}
